8a7a6c07ec7d4d2268724fd3004280f1a15c604f
I A <ilie.adrian1986@gmail.com>
2024-01-15 16:33:24 +0200

    added simple basic validation to check if the passwords are the same

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello world")
+const btn = document.getElementById('button');
+const pswd = document.getElementById('password');
+const pswdConfirm = document.getElementById('password-confirm');
+const errorMessage = document.getElementById("error-message");
+
+
+function checkPassword() {
+    if(pswd.value !== pswdConfirm.value) {
+        errorMessage.classList.add("display-error");
+        pswd.classList.add("error");
+        pswdConfirm.classList.add("error");
+    } else {
+        errorMessage.classList.remove("display-error");
+        pswd.classList.remove("error");
+        pswdConfirm.classList.remove("error");
+    } 
+}
+
+btn.addEventListener("click", () => {
+    event.preventDefault();
+    checkPassword()
+});
